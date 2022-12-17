837898fc703ed6678931d1ee55880f448e8a89ff
francis-100 <85951839+francis-100@users.noreply.github.com>
2022-12-17 15:37:02 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9,
-  * followed by a new line
-   */
-   void print_numbers(void)
-   {
-   	int i;
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
 
-		for (i = 0; i < 10; i++)
-			{
-					_putchar(i + '0');
-						}
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
 
-							_putchar('\n');
-							}
+	_putchar('\n');
+}
