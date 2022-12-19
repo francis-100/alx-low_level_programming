09346aa6421fdfd36c10a02171ca3e14e48d1081
francis-100 <85951839+francis-100@users.noreply.github.com>
2022-12-19 17:30:32 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,20 @@
 
 /**
  * puts2 - prints every character of a string
-  *
-   * @str: string parameter input
-    *
-	 * Return: Noting
-	 */
+ *
+ * @str: string parameter input
+ *
+ * Return: Noting
+*/
 
-	 void puts2(char *str)
-	 {
-	 	int i;
+void puts2(char *str)
+{
+	int i;
 
-			for (i = 0; str[i] != '\0'; ++i)
-				{
-						if (i % 2 == 0)
-									_putchar(str[i]);
-										}
-											_putchar('\n');
-											}
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
