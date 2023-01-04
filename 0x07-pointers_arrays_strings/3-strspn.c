9d028060d688ef60ee40fa6efbb730622cec542b
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-04 21:33:58 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,33 @@
+#include "main.h"
+
 /**
- * _strspn - a function that gets the
- *           length of a prexif substring
+ * _strspn - function that gets length of a prexif substring
  *
  * @s: pointer to string input
  * @accept: substring prefix to look for
  *
- * Return: the number of bytes in the initial segment
+ * Return: number of bytes in the initial segment
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f;
+	int i, j, k;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
-		f = 1; /*flag status*/
+		k = 1;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
-				f = 0; /*success*/
+				k = 0;
 				break;
 			}
 			j++;
 		}
-		if (f == 1)
+		if (k == 1)
 			break;
 		i++;
 	}
