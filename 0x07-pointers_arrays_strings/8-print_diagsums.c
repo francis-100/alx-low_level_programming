cd76a36cf3d090720483f07c2829bbb4570e2e0f
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-04 22:58:34 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,17 +11,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, s1, s;
+	int i, S1, S2;
 
-	s1 = 0;
-	s2 = 0;
+	S1 = 0;
+	S2 = 0;
 
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
-			s1 += a[i];
+			S1 += a[i];
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			s2 += a[i];
+			S2 += a[i];
 	}
-	printf("%d, %d\n", s1, s2);
+	printf("%d, %d\n", S1, S2);
 }
