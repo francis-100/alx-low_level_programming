b4d3111193dc79af04928bc4d303c5ce209dd900
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-06 13:16:22 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,9 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * @n: number of times the backslash \ should be printed
  */
+
 void print_diagonal(int n)
 {
 	if (n <= 0)
