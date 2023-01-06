163518f0be15d9a7c5a232b9e5e4b485ce677227
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-06 10:33:41 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the size of various data types
+ *
+ * Return: 0 if successful, non-zero otherwise
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
