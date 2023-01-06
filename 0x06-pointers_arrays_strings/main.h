5a280d6c4be04ab80a7896d50aa5d6481085f6ea
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-06 17:08:55 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +12,5 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+endif
