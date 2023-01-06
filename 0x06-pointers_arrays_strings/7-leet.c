ebc43a3fee0cb27cbc25a6eff13021026329df11
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-06 17:07:27 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,7 @@
+#include "main.h"
+
 /**
- * leet - a function that encodes a string into 1337
- *
+ * leet - function that encodes a string into 1337
  * @s: string input
  *
  * Return: @s
@@ -13,7 +14,6 @@ char *leet(char *s)
 	int ul[] = {65, 69, 79, 84, 76};
 	int n[] = {52, 51, 48, 55, 49};
 
-	/*iterate values in array s*/
 	while (s[c] != '\0')
 	{
 		/**
@@ -23,6 +23,7 @@ char *leet(char *s)
 		 * upper letter in array ul and if it
 		 * is replace it with the value in array n
 		*/
+
 		for (i = 0; i < 5; i++)
 		{
 			if (s[c] == sl[i] || s[c] == ul[i])
