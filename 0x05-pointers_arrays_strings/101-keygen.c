42328530a28ddd2937d30b4025c8f5f1a6a38369
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-06 15:59:06 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * main - generates random valid
+ * passwords for 101-crackme
  *
  * Return: Always 0 (Success)
  */
