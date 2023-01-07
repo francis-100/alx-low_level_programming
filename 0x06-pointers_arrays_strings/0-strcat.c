4d461721875aac7565dbd056f03f02510f0022b5
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-07 12:06:12 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates two strings
+ * _strcat - function that concatenates
+ *          two strings
  * @dest: pointer to destnation input
  * @src: pointer to source input
  *
@@ -11,12 +12,14 @@
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
+
 	c = 0;
 
 	while (dest[c])
 		c++;
 
 	for (c2 = 0; src[c2] ; c2++)
+
 		dest[c++] = src[c2];
 
 	return (dest);
