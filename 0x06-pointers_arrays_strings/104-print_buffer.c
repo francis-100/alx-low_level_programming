3d9e90fe3ca82260bb0cbabfccb6f99881a7cfae
francis-100 <85951839+francis-100@users.noreply.github.com>
2023-01-07 13:04:46 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,61 +2,47 @@
 #include <stdio.h>
 
 /**
- * isPrintableASCII - checks if n is a printable ASCII char
- * @n: integer
- *
- * Return: 1 if true, 0 if false
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
  */
-
-int isPrintableASCII(int n)
-{
-	return (n >= 32 && n <= 126);
-}
-
-void printHexes(char *b, int start, int end)
-{
-	int i = 0;
-
-	while (i < 10)
-	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
-		i++;
-	}
-}
-
-void printASCII(char *b, int start, int end)
-{
-	int ch, i = 0;
-
-	while (i < end)
-	{
-		ch = *(b + i + start);
-		if (!isPrintableASCII(ch))
-			ch = 46;
-		printf("%c", ch);
-		i++;
-	}
-}
 
 void print_buffer(char *b, int size)
 {
-	int start, end;
+	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		for (start = 0; start < size; start += 10)
+		for (i = 0; i < size; i += 10)
 		{
-			end = (size - start < 10) ? size - start : 10;
-			printf("%08x: ", start);
-			printHexes(b, start, end);
-			printASCII(b, start, end);
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
+			{
+				if (j % 2 == 0 && j != 0)
+					printf(" ");
+				if ((j + i) > size - 1)
+				{
+					printf("  ");
+				}
+				else
+					printf("%.2x", b[i + j]);
+				j++;
+			}
+			printf(" ");
+			j = 0;
+			while (j < 10)
+			{
+				if ((j + i) > size - 1)
+					break;
+				if (b[j + i] <= 31 || b[j + 1] >= '~')
+					b[j + i] = '.';
+				putchar(b[j + i]);
+				j++;
+			}
 			printf("\n");
 		}
-	} else
-		printf("\n");
+	}
 }
